aa303850e4e7b461612ed334b0cee120a1cb99f8
pcbennett108 <pcbennett42@gmail.com>
2023-10-19 17:34:09 -0600

    Add factories

---
+++ spec/factories/items.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :item do
+    merchant
+    name { Faker::Commerce.product_name }
+    description { Faker::Lorem.sentence }
+    unit_price { Faker::Commerce.price }
+  end
+end
